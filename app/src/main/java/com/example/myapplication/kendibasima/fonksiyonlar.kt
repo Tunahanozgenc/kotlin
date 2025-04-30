0e843f51be3b8079bbc0d91f7b3d4d7f54bd9243
Tunahan <ozgenctunahan@gmail.com>
2025-04-30 18:01:20 +0300

    constructor

--- app/src/main/java/com/example/myapplication/kendibasima/fonksiyonlar.kt
+++ app/src/main/java/com/example/myapplication/kendibasima/fonksiyonlar.kt
@@ -2,6 +2,10 @@ package com.example.myapplication.kendibasima
 
 fun main() {
 
+    //primaryConstructor
+    var superman = supeKahraman("Walter White",21,"Kimyacı")
+    var batman = supeKahraman("Declan Rice",29,"Futbolcu")
+
 
 
 
---
+++ app/src/main/java/com/example/myapplication/kendibasima/supeKahraman.kt
@@ -0,0 +1,6 @@
+package com.example.myapplication.kendibasima
+
+class supeKahraman(isim:String,yas:Int,meslek:String) {
+
+
+}
